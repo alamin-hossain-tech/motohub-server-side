fafbf54c6c82a46c5a53654705f152003bd3456a
Al-Amin Hossain <alaminnishat@gmail.com>
2022-12-02 19:55:57 +0600

    Cors origin fixed

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 //mongodbapi
